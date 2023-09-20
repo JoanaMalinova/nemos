59f684f294148d2fef12fd0e108591788876c9e9
Joana Malinova <ani.mani.nen@gmail.com>
2023-09-20 15:23:08 +0300

    added storeService

--- src/services/stroreService.js
+++ src/services/stroreService.js
@@ -1,19 +1,44 @@
 import { firebaseApp } from "../firebase_setup/firebase";
 import { getFirestore } from "firebase/firestore";
+import { collection, getDocs, query, where } from "firebase/firestore";
 
 const db = getFirestore(firebaseApp);
 
 
-export const getAllItems = () => {
+export const getAllItems = async () => {
+
+    const result = [];
+
+    const querySnapshot = await getDocs(collection(db, "store"));
+
+    querySnapshot.forEach((doc) => {
+        result.push(doc.data());
+    });
 
 }
 
-export const getAllFish = () => {
+export const getAllFish = async () => {
+
+    const q = query(collection(db, "cities"), where("capital", "==", true));
+
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        console.log(doc.id, " => ", doc.data());
+    });
 
 }
 
 
-export const getAllInvertebrate = () => {
+export const getAllInvertebrate = async () => {
+
+    const q = query(collection(db, "cities"), where("capital", "==", true));
+
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        console.log(doc.id, " => ", doc.data());
+    });
 
 }
 
