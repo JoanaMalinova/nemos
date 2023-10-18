0dca38a5db50e37a4afa5e1722e766eebb4c2e89
Yoana Malinova <110969425+JoanaMalinova@users.noreply.github.com>
2023-10-18 15:10:11 +0300

    consider and why choose us section are responsive

--- src/components/home/consider section/Consider.js
+++ src/components/home/consider section/Consider.js
@@ -4,30 +4,30 @@ function Consider() {
 
     return (
         <section className={styles.consider}>
-            <h2>Not Covinced Yet?</h2>
-            <ul>
-                <li>
-                    <div>
+            <div className={styles["inner-wrapper"]}>
+                <h2 className={styles["home-header"]}>Not Covinced Yet?</h2>
+                <div className={styles["icon-wrapper"]}>
+                    <div className={styles["icon-holder"]}>
                         <i className="fa-solid fa-truck-fast fa-2xl"></i>
                         <p>Fast Door to Door Shipping</p>
                     </div>
-                </li>
-                <li>
-                    <div>
+                    <div className={styles["icon-holder"]}>
                         <i className="fa-solid fa-truck fa-2xl"><span className={styles.free}>FREE</span></i>
                         <p>Free Shipping over $300</p>
                     </div>
-                </li>
-                <li>
-                    <div className={styles["secure-checkout"]}>
+                    <div className={styles["icon-holder"]}>
                         <i className="fa-solid fa-shield fa-2xl"><i className="fa-solid fa-cart-shopping"></i></i>
                         <p>Secure Checkout</p>
                     </div>
-                </li>
-
-            </ul>
+                </div>
+            </div>
         </section>
     )
 }
 
 export default Consider;
+
+
+
+
+
