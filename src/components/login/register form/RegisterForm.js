ec520493a66164f65977d246fa9fee6614065858
Yoana Malinova <110969425+JoanaMalinova@users.noreply.github.com>
2023-10-24 17:04:51 +0300

    added validation to register form with formik and yup

--- src/components/login/register form/RegisterForm.js
+++ src/components/login/register form/RegisterForm.js
@@ -1,37 +1,68 @@
 import classes from "../login form/LoginForm.module.css";
-import { Link, Form } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { Formik, Form, Field } from "formik";
+import { userRegisterSchema } from "../../../utils/validationSchema";
 
 
 function RegisterForm() {
+
+    const initialValues = {
+        "first-name": "",
+        "last-name": "",
+        email: "",
+        password: "",
+        newsletter: ""
+    }
+
+    const onRegisterSubmit = (values) => {
+        console.log(values);
+    }
 
     return (
         <div className={classes["form-wrapper"]} >
             <h1>Create my account</h1>
             <h4>Please fill in the information below:</h4>
-            <Form method="POST">
-                <div className={classes["input-wrapper"]}>
-                    <label htmlFor="first-name">First name</label>
-                    <input type="text" id="first-name" name="first-name" />
-                </div>
-                <div className={classes["input-wrapper"]}>
-                    <label htmlFor="last-name">Last name</label>
-                    <input type="text" id="last-name" name="last-name" />
-                </div>
-                <div className={classes["input-wrapper"]}>
-                    <label htmlFor="email">Email</label>
-                    <input type="text" id="email" name="id" />
-                </div>
-                <div className={classes["input-wrapper"]}>
-                    <label htmlFor="password">Password</label>
-                    <input type="password" id="password" name="password" />
-                </div>
-                <div>
-                    <input type="checkbox" id="newsletter" name="newsletter" value="subscribed" />
-                    <label htmlFor="newsletter">Subscribe to our newsletter?</label>
-                </div>
-                <button type="submit">Sign up</button>
-                <p>Returning Customer? <Link to="/login">Sign in</Link></p>
-            </Form>
+            <Formik
+                initialValues={initialValues}
+                validationSchema={userRegisterSchema}
+                onSubmit={onRegisterSubmit}
+            >
+                {({ errors, touched }) => (
+                    <Form method="POST">
+                        <div className={classes["input-wrapper"]}>
+                            <label htmlFor="first-name">First name</label>
+                            <Field type="text" id="first-name" name="first-name" />
+                            {errors["first-name"] && touched["first-name"] ?
+                                <div className={classes["error-div"]}>{errors["first-name"]}</div> : null}
+                        </div>
+                        <div className={classes["input-wrapper"]}>
+                            <label htmlFor="last-name">Last name</label>
+                            <Field type="text" id="last-name" name="last-name" />
+                            {errors["last-name"] && touched["last-name"] ?
+                                <div className={classes["error-div"]}>{errors["last-name"]}</div> : null}
+                        </div>
+                        <div className={classes["input-wrapper"]}>
+                            <label htmlFor="email">Email</label>
+                            <Field type="text" id="email" name="email" />
+                            {errors.email && touched.email ?
+                                <div className={classes["error-div"]}>{errors.email}</div> : null}
+                        </div>
+                        <div className={classes["input-wrapper"]}>
+                            <label htmlFor="password">Password</label>
+                            <Field type="password" id="password" name="password" />
+                            {errors.password && touched.password ?
+                                <div className={classes["error-div"]}>{errors.password}</div> : null}
+                        </div>
+                        <div>
+                            <Field type="checkbox" id="newsletter" name="newsletter" value="subscribed" />
+                            <label htmlFor="newsletter">Subscribe to our newsletter?</label>
+                        </div>
+                        <button type="submit">Sign up</button>
+                        <p>Returning Customer? <Link to="/login">Sign in</Link></p>
+                    </Form>
+                )}
+            </Formik>
+
         </div >
     )
 }
--- src/utils/validationSchema.js
+++ src/utils/validationSchema.js
@@ -1,14 +1,31 @@
 import { object, string } from 'yup';
-
-const passwordRules = /^(?=.*([A-Z]){1,})(?=.*[!@#$&*]{1,})(?=.*[0-9]{1,})(?=.*[a-z]{1,}).{8,25}$/;
 const firstNameRules = /[a-zA-Z]/;
 const lastNameRules = /[a-zA-Z]/;
+const latinLettersLowerRules = /.*[a-z]{1,}/;
+const latinLettersUpperRules = /.*([A-Z]){1,}/;
+const specialCharsRules = /.*[!@#$&*]{1,}/;
+const numberRules = /.*[0-9]{1,}/;
 
 export const userRegisterSchema = object({
-    "first-name": string().required("Password is required").matches(firstNameRules, "Unallowed character").min(2, "First name should be at least two letters long"),
-    "last-name": string().required("Password is required").matches(lastNameRules, "Unallowed character").min(2, "Last name should be at least two letters long"),
-    email: string().email("This does not look like an email!").required("Email is required"),
-    password: string().required("Password is required").matches(passwordRules, "Password is not strong enough")
+    "first-name": string()
+        .required("First name is required")
+        .matches(firstNameRules, "Unallowed character")
+        .min(2, "First name should be at least two letters long"),
+    "last-name": string()
+        .required("Last name is required")
+        .matches(lastNameRules, "Unallowed character")
+        .min(2, "Last name should be at least two letters long"),
+    email: string()
+        .required("Email is required")
+        .email("This does not look like an email!"),
+    password: string()
+        .required("Password is required")
+        .min(8, "Password should be at least 8 characters long")
+        .max(25, "Password should be no more than 25 characters long")
+        .matches(latinLettersLowerRules, "Password should contain at least one latin letter in lowercase")
+        .matches(latinLettersUpperRules, "Password should contain at least one latin letter in uppercase")
+        .matches(numberRules, "Password should contain at least one number")
+        .matches(specialCharsRules, "Password should contain at least one special character (!@#$&*)")
 });
 
 export const userLoginSchema = object({
