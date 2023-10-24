303d2380cc2571ec9d3213b01f13c9a24c09aeae
Yoana Malinova <110969425+JoanaMalinova@users.noreply.github.com>
2023-10-24 16:29:28 +0300

    added basic validation to login form with formi and yup

--- src/components/login/login form/LoginForm.js
+++ src/components/login/login form/LoginForm.js
@@ -1,25 +1,54 @@
-import { Link } from "react-router-dom";
 import classes from "./LoginForm.module.css";
+import { Link } from "react-router-dom";
+import { Formik, Form, Field } from "formik";
+import { userLoginSchema } from "../../../utils/validationSchema";
 
 function LoginForm() {
+
+    const initialValues = { email: "", password: "" };
+    const onLoginSubmit = (values) => {
+        console.log(values)
+    }
 
     return (
         <div className={classes["form-wrapper"]}>
             <h1>Customer Login</h1>
             <h4>Enter your email and password</h4>
-            <form>
-                <div className={classes["input-wrapper"]}>
-                    <label htmlFor="email">Email</label>
-                    <input type="text" id="email" name="email" />
-                </div>
-                <div className={classes["input-wrapper"]}>
-                    <label htmlFor="password">Password</label>
-                    <input type="password" id="password" name="password" />
-                </div>
-                <p><a href="/reset-email">Forgot your password?</a></p>
-                <button type="submit">Login</button>
-                <p>New customer? <Link to="/register">Create an account</Link></p>
-            </form>
+            <Formik
+                initialValues={initialValues}
+                validationSchema={userLoginSchema}
+                onSubmit={onLoginSubmit}
+            >
+                {({ errors, touched }) => (
+                    <Form>
+                        <div className={classes["input-wrapper"]}>
+                            <label htmlFor="email">Email</label>
+                            <Field
+                                type="text"
+                                id="email"
+                                name="email"
+                                placeholder="didi@abv.bg"
+                            />
+                            {errors.email && touched.email ?
+                                <div className={classes["error-div"]}>{errors.email}</div> : null}
+                        </div>
+                        <div className={classes["input-wrapper"]}>
+                            <label htmlFor="password">Password</label>
+                            <Field
+                                type="password"
+                                id="password"
+                                name="password"
+                                placeholder="********"
+                            />
+                            {errors.password && touched.password ?
+                                <div className={classes["error-div"]}>{errors.password}</div> : null}
+                        </div>
+                        <p><a href="/reset-email">Forgot your password?</a></p>
+                        <button type="submit">Login</button>
+                        <p>New customer? <Link to="/register">Create an account</Link></p>
+                    </Form>
+                )}
+            </Formik>
         </div>
 
     )
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,6 +2,7 @@ import { json, redirect } from "react-router-dom";
 import LoginDeals from "../components/login/login deals/LoginDeals";
 import LoginForm from "../components/login/login form/LoginForm";
 import { signIn } from "../services/authService";
+import { userLoginSchema } from "../utils/validationSchema";
 
 function LoginPage() {
 
@@ -16,18 +17,21 @@ function LoginPage() {
 export default LoginPage;
 
 export const loginAction = async ({ request }) => {
+    console.log("im in")
 
     const data = await request.formData();
 
     const email = data.get("email");
     const password = data.get("password");
+    const something = await userLoginSchema.validate({ email, password });
+    console.log(something);
 
-    const user = await signIn(email, password);
-    if (user.error) {
-        console.log(user.error)
-        throw json({ message: user.error }, { status: 401 });
-    }
+    // const user = await signIn(email, password);
+    // if (user.error) {
+    //     console.log(user.error)
+    //     throw json({ message: user.error }, { status: 401 });
+    // }
 
-    return redirect('/store');
+    // return redirect('/store');
 
 }
