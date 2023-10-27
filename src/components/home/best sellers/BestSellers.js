76a647d62e64eea3330c8f9e1024c1f8d07e91f7
Yoana Malinova <110969425+JoanaMalinova@users.noreply.github.com>
2023-10-27 15:50:47 +0300

    adjusted card and arrivals for small screens

--- src/components/home/best sellers/BestSellers.js
+++ src/components/home/best sellers/BestSellers.js
@@ -1,11 +1,13 @@
 import { useLoaderData, useNavigate } from "react-router-dom";
 import styles from "./BestSellers.module.css";
 import BestSellerCard from "./best seller card/BestSellerCard";
+import { useState } from "react";
 
 function BestSellers() {
 
     const { items } = useLoaderData();
     const navigate = useNavigate();
+    const [isClicked, setIsClicked] = useState(items.map(curr => curr = { id: curr.id, clicked: false }));
 
     const onBttnClick = () => {
         navigate("/store");
@@ -17,7 +19,12 @@ function BestSellers() {
                 <h2 className={styles["home-header"]}>Best Sellers</h2>
             </div>
             <div className={styles.container}>
-                {items.map(item => <BestSellerCard key={item.id} item={item} />)}
+                {items.map(item => <BestSellerCard
+                    key={item.id}
+                    item={item}
+                    isClicked={isClicked}
+                    setIsClicked={setIsClicked}
+                />)}
             </div>
             <button className="purple-btn" onClick={onBttnClick}>View all</button>
         </section>
